7ed56dad0f722aae41847a69d9ac1d7e650ad0b4
Anna Petrone <apetrone@netflix.com>
2020-12-12 15:49:22 -0500

    add congnito config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_C5TCVmi8t', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3qi8tbb8234d6c66bgqjo065m3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
