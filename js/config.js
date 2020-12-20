27c493dcf47397669c12655c0349d99288bfe03a
Anna Petrone <apetrone@netflix.com>
2020-12-20 15:23:51 -0500

    udpate js config for amplify user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_C5TCVmi8t', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3qi8tbb8234d6c66bgqjo065m3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_EsLHBsLaZ',
+        userPoolClientId: '4tori2opsps4bjp5b6etvg1aeb',
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: 'https://xce0pika3a.execute-api.us-east-1.amazonaws.com/prod'
