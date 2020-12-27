df3177791a5cbfca96d78cd8e5709a4c099ee14e
ajtmccarty <aaron.mccarty@packetfabric.com>
2020-12-27 16:17:10 -0500

    Add a better CLI with argparse

--- python-core/cli.py
+++ python-core/cli.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
 import time
 from pathlib import Path
 
@@ -5,11 +10,39 @@ from config import HERE_API_KEY
 from geocode.here import HereClient
 
 if __name__ == "__main__":
-    submissions_file = "/home/amccarty/w4ma/test-here.txt"
-    working_dir = Path("/home/amccarty/w4ma/batches/")
+    parser = argparse.ArgumentParser(
+        prog="Batch Geo-coder",
+        description="Geocode addresses in CSV files using the Here API",
+    )
+    parser.add_argument(
+        "working-dir",
+        type=Path,
+        help="absolute or relative path to directory to save result files",
+    )
+    parser.add_argument(
+        "batch-file",
+        type=Path,
+        help="absolute or relative path to batch file to submit for geocoding",
+    )
+
+    args: dict = vars(parser.parse_args())
+    cwd: Path = Path(os.getcwd())
+    working_dir: Path = args["working-dir"]
+    if not working_dir.is_absolute():
+        working_dir = cwd / working_dir
+    if not (working_dir.exists() and working_dir.is_dir()):
+        print(f"Invalid working-dir '{working_dir}'. It must exist and be a directory.")
+        sys.exit(1)
+
+    batch_file: Path = args["batch-file"]
+    if not batch_file.is_absolute():
+        batch_file = cwd / batch_file
+    if not (batch_file.exists() and batch_file.is_file()):
+        print(f"Invalid batch '{batch_file}'. It must exist and be a file.")
+        sys.exit(1)
 
     client = HereClient(HERE_API_KEY, working_dir)
-    request_id = client.send_batch(submissions_file)
+    request_id = client.send_batch(batch_file)
     for i in range(30):
         status = client.get_job_status(request_id)
         if status.lower() == "completed":
